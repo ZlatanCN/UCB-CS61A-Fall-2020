9c5be5ce8415475663e407917e5172f8f637b9c0
Jiayou Zhu <43867657+ZlatanCN@users.noreply.github.com>
2023-08-06 18:14:31 +0800

    disc06 finished

---
+++ discussion/disc06/disc06.py
@@ -0,0 +1,99 @@
+def memory(n):
+    """
+    >>> f = memory(10)
+    >>> f(lambda x: x * 2)
+    20
+    >>> f(lambda x: x - 7)
+    13
+    >>> f(lambda x: x > 5)
+    True
+    """
+    def f(g):
+        nonlocal n
+        n = g(n)
+        return n
+    return f
+
+
+def group_by(s, fn):
+    """
+    >>> group_by([12, 23, 14, 45], lambda p: p // 10)
+    {1: [12, 14], 2: [23], 4: [45]}
+    >>> group_by(range(-3, 4), lambda x: x * x)
+    {9: [-3, 3], 4: [-2, 2], 1: [-1, 1], 0: [0]}
+    """
+    grouped = {}
+    for elem in s:
+        key = fn(elem)
+        if key in grouped:
+            grouped[key].append(elem)
+        else:
+            grouped[key] = [elem]
+    return grouped
+
+
+def add_this_many(x, el, s):
+    """ Adds el to the end of s the number of times x occurs
+    in s.
+    >>> s = [1, 2, 4, 2, 1]
+    >>> add_this_many(1, 5, s)
+    >>> s
+    [1, 2, 4, 2, 1, 5, 5]
+    >>> add_this_many(2, 2, s)
+    >>> s
+    [1, 2, 4, 2, 1, 5, 5, 2, 2]
+    """
+    cnt = 0
+    for i in s:
+        if i == x:
+            cnt += 1
+    while cnt:
+        s.append(el)
+        cnt -= 1
+
+
+def filter(iterable, fn):
+    """
+    >>> is_even = lambda x: x % 2 == 0
+    >>> list(filter(range(5), is_even)) # a list of the values yielded from the call to filter
+    [0, 2, 4]
+    >>> all_odd = (2*y-1 for y in range(5))
+    >>> list(filter(all_odd, is_even))
+    []
+    >>> naturals = (n for n in range(1, 100))
+    >>> s = filter(naturals, is_even)
+    >>> next(s)
+    2
+    >>> next(s)
+    4
+    """
+    for i in iterable:
+        if fn(i):
+            yield i
+
+
+def merge(a, b):
+    """
+    >>> def sequence(start, step):
+    ...     while True:
+    ...         yield start
+    ...         start += step
+    >>> a = sequence(2, 3) # 2, 5, 8, 11, 14, ...
+    >>> b = sequence(3, 2) # 3, 5, 7, 9, 11, 13, 15, ...
+    >>> result = merge(a, b) # 2, 3, 5, 7, 8, 9, 11, 13, 14, 15
+    >>> [next(result) for _ in range(10)]
+    [2, 3, 5, 7, 8, 9, 11, 13, 14, 15]
+    """
+    el_a = next(a)
+    el_b = next(b)
+    while True:
+        if el_a < el_b:
+            yield el_a
+            el_a = next(a)
+        elif el_b < el_a:
+            yield el_b
+            el_b = next(b)
+        else:
+            yield el_a
+            el_a = next(a)
+            el_b = next(b)
