61b19ae841881c7daea86d22d7d84d51d5c64f64
Jiayou Zhu <43867657+ZlatanCN@users.noreply.github.com>
2023-08-08 18:09:54 +0800

    lab07 finished

---
+++ lab/lab07/lab07.py
@@ -0,0 +1,54 @@
+def naturals():
+    """A generator function that yields the infinite sequence of natural
+    numbers, starting at 1.
+
+    >>> m = naturals()
+    >>> type(m)
+    <class 'generator'>
+    >>> [next(m) for _ in range(10)]
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    """
+    i = 1
+    while True:
+        yield i
+        i += 1
+
+
+def scale(it, multiplier):
+    """Yield elements of the iterable it scaled by a number multiplier.
+
+    >>> m = scale([1, 5, 2], 5)
+    >>> type(m)
+    <class 'generator'>
+    >>> list(m)
+    [5, 25, 10]
+
+    >>> m = scale(naturals(), 2)
+    >>> [next(m) for _ in range(5)]
+    [2, 4, 6, 8, 10]
+    """
+    "*** YOUR CODE HERE ***"
+    yield from (x * multiplier for x in it)
+
+
+def hailstone(n):
+    """
+    >>> for num in hailstone(10):
+    ...     print(num)
+    ...
+    10
+    5
+    16
+    8
+    4
+    2
+    1
+    """
+    "*** YOUR CODE HERE ***"
+    yield n
+    if n == 1:
+        return
+    if n % 2 == 0:
+        yield from (even for even in hailstone(n // 2))
+    else:
+        yield from (odd for odd in hailstone(n * 3 + 1))
